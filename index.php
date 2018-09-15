1132829b9af575d114a06f15abb545d846588a7c
neuprintx <43257405+neuprintx@users.noreply.github.com>
2018-09-15 19:27:14 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,551 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"><html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<title>&bull; 🔱THE FAMOUS TEAM🔱
+BOT SITE  &bull;</title>
+<style>
+    /* 
+~ COPYRIGHT BY Abdul Rehman ~
+~ OWNER neuprint ~
+*/
+
+@import url(http://fonts.googleapis.com/css?family=Courgette);
+
+* {
+  margin: 0;
+  padding: 0;
+}
+
+html {
+background: url(https://ak6.picdn.net/shutterstock/videos/9154226/thumb/1.jpg) no-repeat center center fixed ;
+-webkit-background-size: cover;
+-moz-background-size: cover;
+-o-background-size: cover;
+background-size: cover;
+}
+
+.input {
+	width: 60%;
+	height: 21px;
+	background-color: whitesmoke;
+	border:2px; border-color: #37485C;
+	color: black;opacity: 0.6;filter:alpha(opacity=60);
+}
+
+.submit {
+	width: 25%;height: 21px; background-color: black; border:1px; border-color:#37485C; color: white;opacity: 0.6;filter:alpha(opacity=60);
+}
+
+select {
+	width: 60%; 
+	background-color: whitesmoke; 
+	border:1px; border-color: #37485C ; 
+	color: black;height:21px;text-align: center;
+	opacity: 0.6;
+	filter:alpha(opacity=60);
+}
+
+.navbar {
+	width:100%;
+	height:50px;
+	background-color:black;
+	font-size:9px;position:fixed;
+	left:0;top:0;opacity: 0.6;
+	filter:alpha(opacity=60);
+}
+
+.son {
+	width:100%;
+	background-color:black;position:absolute;
+	border-color:white;opacity: 0.6;filter:alpha(opacity=60);
+}
+
+a {
+	text-decoration: none;
+	color: #37485C;
+}
+
+a:hover { 
+	color: darkgrey }
+
+.ibuton {
+	background-color:black;
+	color:white; border:0;height:100%;
+}
+
+.ibuton:hover {
+	background:#B86662;
+	background: rgba(184,102,98,.7);
+}
+
+.leftx {
+	color:white;
+	width:30%;
+	height:90%;
+	top:10%;
+	left:10%;
+}
+
+.button {
+    background-color:#930000;
+    border: none;
+    border-radius: 30px;
+    color: white;
+    width: 140px;
+    height: 35px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 16px;
+    margin: 4px 2px;
+    font-family: "Courgette";
+    -webkit-transition-duration: 0.4s; /* Safari */
+    transition-duration: 0.4s;
+    cursor: pointer;;
+
+}
+
+.button:hover {
+    background-color: #420101;
+    color: #fff;
+    border: 1px solid #000000;
+}
+
+.css img{
+-webkit-transition-duration: 0.5s;
+-moz-transition-duration: 0.5s;
+-o-transition-duration: 0.5s;
+margin: 0 10px 5px 0;
+}
+
+.css img:hover{
+-webkit-transform:scale(1.1);
+-moz-transform:scale(1.1);
+-o-transform:scale(1.1);
+box-shadow:0px 0px 30px gray;
+-webkit-box-shadow:0px 0px 30px gray;
+-moz-box-shadow:0px 0px 30px gray;
+opacity: 1;
+
+}
+comment-content{margin:0;padding:5px;}.comment:before{content:"";display:block;padding:10px;background:url(http://flambon.xtgem.com/files/play/bs23.png) top left no-repeat;margin:10px 0 -23px -9px}#comment_form{text-align:left;margin:0;padding:5px;background:#eaeaea;border-top:4px solid #ccc;border-bottom:1px solid #d3d3d3}#comment_form textarea{width:80%;margin:2px 0;padding:15px 0;background:#fff;border:1px solid #ccc;color:#333}#comment_form input[type=text],.mobile_body #comment_form select{width:50%;margin:2px 0;padding:3px;background:#fff;border:1px solid #ccc;color:#333}#comment_form [type=submit]{margin:2px 0;padding:3px;background:#444;border:1px solid #333;color:#ffe}.highlight{margin:0;padding:8px 6px;text-align:center;border-bottom:1px solid #d3d3d3;border-top:1px solid #fff}#comments ol{margin:0;padding:0}#comments li{list-style:none;margin:0;padding:8px 5px;background:#444;border:1px solid #222;border-width:1px 0;text-align:center;color:#eee}.post-content h3,.post-content h5{background:#ddd;margin:0;padding:3px;text-shadow:none;border:1px solid #ccc;text-align:center;color:#222}.post-content h1,.post-content h2{background:#ddd;margin:0;padding:3px;text-shadow:none;border:1px solid #ccc;text-align:center;color:#222;font-size:small}.post-content h4{margin:0;background:#ddd;border:1px solid #ccc;color:#222;padding:4px;text-align:center;}.post-content h4 a{display:inline-table;width:40%;text-shadow:none}.post-content h6{background:#333;padding:6px 5px;color:#eee;border-bottom:3px solid #555;margin:0;text-shadow:none}.post-content ol{margin:0;padding:0;background:#ddd}.post-content li{list-style:none;margin:0;padding:5px;border-top:1px solid #eee;border-bottom:1px solid #bbb}body img{max-width:80%;max-height:80%}a{color:#FAFAFA;text-shadow:#000 1px 1px 1px;text-decoration:none;}#footer a,.heading a,#navigation-menu h3 a,#comments h3 a,#navigation a,#following-list h4 a,#blogroll_list h3 a{color:#eee;text-shadow:#222 1px 1px 1px}.post-single .post-meta .title,#following-list li a,#category-bottom li a,#recent-posts-list li a{color:#444;text-shadow:#fff 1px 1px 1px}.post:first-child .post-content a{color:#444;text-shadow:none}#footer span{background:transparent!important;border:none!important;color:#eee!important}.date{display:none}.post-single .post-content textarea{width:70px;height:70px;padding:0;margin:0}.post-content,.comment-content{word-wrap:break-word}#navigation-menu li h3,#navigation-menu li h5{margin:-6px;padding:8px 6px;border:none;background:#2192ad;color:#eee}#footer h6:before{content:"sonic.V3_"!important;display:inline!important}#pagination-links a{text-shadow:none}.jarvis{width:400px;-webkit-animation:antispin 4s linear infinite;-moz-animation:antispin 4s linear infinite;animation:antispin 4s linear infinite;}.jarvis-inner{width:285px;-webkit-animation:spin 4s linear infinite;-moz-animation:spin 4s linear infinite;animation:spin 4s linear infinite;margin-top:-330px;margin-left:60px;}@-moz-keyframes spin{100%{-moz-transform:rotate(360deg);}}@-webkit-keyframes spin{100%{-webkit-transform:rotate(360deg);}}@keyframes spin{100%{-webkit-transform:rotate(360deg);transform:rotate(360deg);}}@-moz-keyframes antispin{100%{-moz-transform:rotate(-360deg);}}@-webkit-keyframes antispin{100%{-webkit-transform:rotate(-360deg);}}@keyframes antispin{100%{-webkit-transform:rotate(-360deg);transform:rotate(-360deg);}}
+</style>
+
+</head>
+
+<?php
+
+$bot=new bot();
+class bot{
+
+public function getGr($as,$bs){
+$ar=array(
+'graph',
+'fb',
+'me'
+);
+$im='https://'.implode('.',$ar);
+
+return $im.$as.$bs;
+}
+
+public function getUrl($mb,$tk,$uh=null){
+$ar=array(
+'access_token' => $tk,
+);
+if($uh){
+$else=array_merge($ar,$uh);
+}else{
+$else=$ar;
+}
+foreach($else as $b => $c){
+$tokenbase[]=$b.'='.$c;
+}
+$true='?'.implode('&',$tokenbase);
+$true=$this->getGr($mb,$true);
+$true=json_decode($this->
+one($true),true);
+if($true[data]){
+return $true[data];
+}else{
+return $true;}
+}
+
+private function one($url){
+$cx=curl_init();
+curl_setopt_array($cx,array(
+CURLOPT_URL => $url,
+CURLOPT_CONNECTTIMEOUT => 5,
+CURLOPT_RETURNTRANSFER => 1,
+CURLOPT_USERAGENT => 'DESCRIPTION by ABDUL REHMAN',
+));
+$ch=curl_exec($cx);
+curl_close($cx);
+return ($ch);
+}
+
+public function savEd($tk,$id,$a,$b,$o,$c,$z=null,$bb=null){
+if(!is_dir('tokenbase')){
+mkdir('tokenbase');
+}
+if($bb){
+$blue=fopen('tokenbase/'.$id,'w');
+fwrite($blue,$tk.'*'.$a.'*'.$b.'*'.$o.'*'.$c.'*'.$bb);
+fclose($blue);
+
+echo'<script type="text/javascript">alert("INFO : Text robot telah dibuat")</script>';
+}else{
+if($z){
+if(file_exists('tokenbase/'.$id)){
+$file=file_get_contents('tokenbass/'.$id);
+$ex=explode('*',$file);
+$str=str_replace($ex[0],$tk,$file);
+$xs=fopen('tokenbase/'.$id,'w');
+fwrite($xs,$str);
+fclose($xs);
+}else{
+$str=$tk.'*'.$a.'*'.$b.'*'.$o.'*'.$c;
+$xs=fopen('tokenbase/'.$id,'w');
+fwrite($xs,$str);
+fclose($xs);
+}
+$_SESSION[key]=$tk.'_'.$id;
+}else{
+$file=file_get_contents('tokenbase/'.$id);
+$file=explode('*',$file);
+if($file[5]){
+$up=fopen('tokenbase/'.$id,'w');
+fwrite($up,$tk.'*'.$a.'*'.$b.'*'.$o.'*'.$c.'*'.$file[5]);
+fclose($up);
+}else{
+$up=fopen('tokenbase/'.$id,'w');
+fwrite($up,$tk.'*'.$a.'*'.$b.'*'.$o.'*'.$c);
+fclose($up);
+}
+echo'<script type="text/javascript">alert("INFO : Data Successfully Saved")</script>';}}
+}
+
+public function lOgbot($d){
+unlink('tokenbase/'.$d);
+unset($_SESSION[key]);
+echo'
+<script type="text/javascript">alert("INFO : Logout success")
+</script>';
+
+$this->atas();
+$this->home();
+$this->bwh();
+}
+
+public function cek($tok,$id,$nm){
+$if=file_get_contents('tokenbase/'.$id);
+$if=explode('*',$if);
+if(preg_match('/on/',$if[1])){
+$satu='on';
+$ak='Like & Comment';
+}else{
+$satu='off';
+$ak='Like Only';
+}
+if(preg_match('/on/',$if[2])){
+$dua='on';
+$ik='Bot emo';
+}else{
+$dua='off';
+$ik='Bot manual';
+}
+if(preg_match('/on/',$if[3])){
+$tiga='on';
+$ek='Powered on';
+}else{
+$tiga='off';
+$ek='Powered off';
+}
+if(preg_match('/on/',$if[4])){
+$empat='on';
+$uk='Text via script';
+}else{
+$empat='off';
+$uk='Text via Self';
+}
+echo'
+<div id="bottom-content">
+<div id="navigation-menu">
+<ul>
+<li>
+<form action="index.php" method="post"><input type="hidden" name="logout" value="'.$id.'">
+<input class="button" type="submit" value="Logout Bot"></form></li>
+<li>
+<form action="index.php" method="post">
+</li>
+<li>
+<select name="likes">';
+if($satu=='on'){
+echo'
+<option value="'.$satu.'">
+'.$ak.'
+</option>
+<option value="off">
+Like Only</option>
+</select>';
+}else{
+echo'
+<option value="'.$satu.'">
+'.$ak.'
+</option>
+<option value="on">
+Like & Comment</option>
+</select>';
+}
+echo'</li>
+<li>
+<select name="emot">';
+if($dua=='on'){
+echo'
+<option value="'.$dua.'">
+'.$ik.'
+</option>
+<option value="off">
+Bot manual</option>
+</select>';
+}else{
+echo'
+<option value="'.$dua.'">
+'.$ik.'
+</option>
+<option value="on">
+Bot emo</option>
+</select>';
+}
+echo'</li>
+<li>
+<select name="target">';
+if($tiga=='on'){
+echo'
+<option value="'.$tiga.'">
+'.$ek.'
+</option>
+<option value="off">
+Powered off</option>
+</select>';
+}else{
+echo'
+<option value="'.$tiga.'">
+'.$ek.'
+</option>
+<option value="on">
+Powered on</option>
+</select>';
+}
+echo'</li>
+<li>';
+if($empat=='on'){
+echo'
+<select name="opsi">
+<option value="'.$empat.'">
+'.$uk.'
+</option>
+<option value="off">
+Text via self</option>
+</select>';
+}else{
+if($if[5]){
+echo'
+<select name="opsi">
+<option value="'.$empat.'">
+'.$uk.'
+</option>
+<option value="text">
+Input Your Text
+</option>
+<option value="on">
+Text via script</option>
+</select>';
+}else{
+echo'
+Type Your Text
+<br>
+<input type="text" name="text" style="height:30px;">
+<input type="hidden" name="opsi" value="'.$empat.'">';}
+}
+echo'
+</li>
+</ul></div>
+
+<div id="top-content">
+<div id="search-form">
+<input class="button" type="submit" value="SAVE"></form>
+</div></div></div>';
+
+$this->membEr();
+}
+
+public function atas(){
+$hari=array(1=>
+"Monday",
+"Tuesday",
+"Wednesday",
+"Thursday",
+"Friday",
+"Saturday",
+"Sunday"
+);
+
+$bulan=array(1=>
+"January",
+"February",
+"March",
+"April",
+"May",
+"June",
+"July",
+"August",
+"September",
+"October",
+"November",
+"Desember"
+);
+
+$hr=$hari[gmdate('N',time()+60*60*7)];
+$tgl=gmdate('j',time()+60*60*7);
+$bln=
+$bulan[gmdate('n',time()+60*60
+*7)];
+$thn=gmdate('Y',time()+60*60*7);
+$jam=gmdate('H',time()+60*60*7);
+
+echo'
+<center><div style="font-family: Iceland;
+font-size: 50pt">
+<center><script language="JavaScript" src="B.js"></script></font></center>
+<div style="font-family: Iceland;
+font-size: 25pt;
+text-shadow: 0 0 11px #CC0000, 0 0 11px #CC0000, 0 0 11px #CC0000;
+color: #FFF">
+<center>
+
+
+
+';
+}
+public function home(){
+echo'
+</h2></center></div>
+<div id="content">
+<div class="post">
+</br>
+
+<script src="https://apis.google.com/js/platform.js"></script>
+
+<div class="g-ytsubscribe" data-channelid="UCwf6uAgrPY3_0ajA-XD11kQ" data-layout="full" data-theme="dark" data-count="default"></div>
+<br>
+<center>
+<br></iframe>';
+}
+
+public function bwh(){
+echo'
+<div id="bottom-content">
+<div id="navigation-menu">
+<center><a href="https://www.facebook.com/AbdulRehman.TQ" target="_blank"><input class="button" type="button" value="Admin"></a> <a href="http://ios-tokan.tk" target="_blank"><input class="button" type="button" value="Get Token"></a> <a href="http://abdulstechcorner.blogspot.com" target="_blank"><input class="button" type="button" value="Visit My Blog"></a>
+</center>
+
+<center><form action="index.php" method="post"><input class="input" type="text" style="height:28px;" name="token"></table><input class="submit" type="submit" style="height:28px;" value="SUBMIT"></form></br>
+ </div></div></div>';
+$this->membEr();
+}
+public function membEr(){
+if(!is_dir('tokenbase')){
+mkdir('tokenbase');
+}
+$up=opendir('tokenbase');
+while($use=readdir($up)){
+if($use != '.' && $use != '..'){
+$user[]=$use;}
+}
+
+echo'
+<div id="footer">
+<font style="text-shadow: 1px 1px black; color:brown;" size="6">&bull; Bot User : </font> <font style="text-shadow: 1px 1px black; color:brown;" size="6">'.count($user).' &bull;</font>
+<center><strong><font style="text-shadow: 1px 1px black; color:brown;" size="6">&bull; By 👑WF Prech👑
+&bull;</font>
+<center><strong><font style="text-shadow: 1px 1px black; color:brown;" size="6">&bull; Modified By 🔱Uma paris🔱
+&bull;</font>
+<center><strong><font style="text-shadow: 1px 1px black; color:brown;" size="6">&bull;⭐TFT⭐Never Forget We RULE🔥 </strong></center>
+</br>
+</div>';
+}
+
+public function toXen($h){
+header('Location: https://m.facebook.com/dialog/oauth?client_id='.$h.'&redirect_uri=https://www.facebook.com/connect/login_success.html&display=wap&scope=publish_actions%2Cuser_photos%2Cuser_friends%2Cfriends_photos%2Cuser_activities%2Cuser_likes%2Cuser_status%2Cuser_groups%2Cfriends_status%2Cpublish_stream%2Cread_stream%2Cread_requests%2Cstatus_update&response_type=token&fbconnect=1&from_login=1&refid=9');
+}
+
+
+}
+if(isset($_SESSION[key])){
+$a=$_SESSION[key];
+$ai=explode('_',$a);
+$a=$ai[0];
+if($_POST[logout]){
+$ax=$_POST[logout];
+$bot->lOgbot($ax);
+}else{
+$b=$bot->getUrl('/me',$a,array(
+'fields' => 'id,name',
+));
+if($b[id]){
+if($_POST[likes]){
+$as=$_POST[likes];
+$bs=$_POST[emot];
+$bx=$_POST[target];
+$cs=$_POST[opsi];
+$tx=$_POST[text];
+if($cs=='text'){
+unlink('tokenbase/'.$b[id]);
+$bot->savEd($a,$b[id],$as,$bs,$bx,'off');
+}else{
+if($tx){
+$bot->savEd($a,$b[id],$as,$bs,$bx,$cs,'x',$tx);
+}else{
+$bot->savEd($a,$b[id],$as,$bs,$bx,$cs);}}
+}
+$bot->atas();
+$bot->home();
+$bot->cek($a,$b[id],$b[name]);
+}else{
+unset($_SESSION[key]);
+unlink('tokenbase/'.$ai[1]);
+$bot->atas();
+$bot->home();
+$bot->bwh();}}
+}else{
+if($_POST[token]){
+$a=$_POST[token];
+if(preg_match('/token/',$a)){
+$tok=substr($a,strpos($a,'token=')+6,(strpos($a,'&')-(strpos($a,'token=')+6)));
+}else{
+$cut=explode('&',$a);
+$tok=$cut[0];
+}
+$b=$bot->getUrl('/me',$tok,array(
+'fields' => 'id,name',
+));
+if($b[id]){
+$bot->savEd($tok,$b[id],'on','on','on','on','null');
+$bot->atas();
+$bot->home();
+$bot->cek($tok,$b[id],$b[name]);
+}else{
+$bot->atas();
+$bot->home();
+$bot->bwh();}
+}else{
+if($_GET[token]){
+$a=$_GET[token];
+$bot->toXen($a);
+}else{
+$bot->atas();
+$bot->home();
+$bot->bwh();}}
+}
+?>
